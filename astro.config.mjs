229c3715a93569b2f2f2e24dc1126751e1f1c8c3
Amine <mohamedaminediboune@gmail.com>
2023-04-05 22:57:56 +0100

    Added react

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import react from "@astrojs/react";
 
+// https://astro.build/config
 export default defineConfig({
   integrations: [tailwind({
     // Example: Provide a custom path to a Tailwind config file
-    config: { path: './custom-config.cjs' },
-  })],
+    config: {
+      path: './custom-config.cjs'
+    }
+  }), react()]
 });
