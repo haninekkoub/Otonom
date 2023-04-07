56a133ac177461f5723d39c39f9ebf58f3ce2de8
haninekkoub <haninekkoub@gmail.com>
2023-04-07 06:50:52 +0100

    about image animation

---
+++ src/components/About.tsx
@@ -0,0 +1,65 @@
+import About01 from "../assets/About_01.png";
+import About02 from "../assets/About_02.png";
+import { useScroll, motion, useSpring, useTransform } from "framer-motion";
+
+export default function About() {
+  const { scrollYProgress } = useScroll();
+
+  const y1 = useSpring(useTransform(scrollYProgress, [0.3, 0.55], [0, 50]), {
+    stiffness: 100,
+    damping: 30,
+    restDelta: 0.001,
+  });
+  const y2 = useSpring(useTransform(scrollYProgress, [0.4, 0.65], [0, 50]), {
+    stiffness: 100,
+    damping: 30,
+    restDelta: 0.001,
+  });
+
+  return (
+    <motion.section className="bg-darkbackground px-[12.5vw] py-16">
+      <motion.div className="flex justify-center items-end gap-28 pb-16 relative ">
+        <motion.div className="w-[30vw] h-[30vw] mt-16 relative overflow-hidden">
+          <motion.div
+            className="w-[35vw] h-[35vw]  bg-no-repeat bg-cover absolute bottom-0"
+            style={{ y: y1, backgroundImage: `url(${About01})` }}
+          />
+        </motion.div>
+        <div>
+          <h2 className="text-aboutTitle text-white font-bold  w-[36.6vw] absolute top-0 left-1/2 transform -translate-x-1/2">
+            Elevating Your Brand with Immersive Web Experiences
+          </h2>
+          <h5 className="text-lg leading-[160%] text-[#D9DDF2] text-justify w-[36vw] leading-[160%]">
+            {" "}
+            At Otonom, we specialize in creating unique and immersive web
+            experiences that help modern brands stand out in the digital world.
+            Our focus on customization ensures that each project we undertake is
+            tailored to meet the unique needs of our clients, resulting in a web
+            experience that is truly one-of-a-kind.{" "}
+          </h5>
+        </div>
+      </motion.div>
+      <motion.div className="flex flex-row-reverse justify-center items-end gap-28 relative">
+        <motion.div className="w-[30vw] h-[30vw] mt-16 relative overflow-hidden">
+          <motion.div
+            className="w-[35vw] h-[35vw]  bg-no-repeat bg-cover absolute bottom-0"
+            style={{ y: y2, backgroundImage: `url(${About02})` }}
+          />
+        </motion.div>
+
+        <div>
+          <h2 className="text-aboutTitle text-white font-bold w-[30vw] text-right absolute top-0 left-1/2 transform -translate-x-1/2">
+            Tailored Web Design Solutions for Modern Brands
+          </h2>
+          <h5 className="text-lg leading-[160%] text-[#D9DDF2] text-justify w-[36vw] leading-[160%]">
+            At Otonom, we specialize in creating unique and immersive web
+            experiences that help modern brands stand out in the digital world.
+            Our focus on customization ensures that each project we undertake is
+            tailored to meet the unique needs of our clients, resulting in a web
+            experience that is truly one-of-a-kind.{" "}
+          </h5>
+        </div>
+      </motion.div>
+    </motion.section>
+  );
+}
--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,72 +1,99 @@
-import banner_first from "../assets/banner_first.png"
-import banner_second from "../assets/banner_second.png"
-import banner_third from "../assets/banner_third.png"
+import banner_first from "../assets/banner_first.png";
+import banner_second from "../assets/banner_second.png";
+import banner_third from "../assets/banner_third.png";
 
 import { motion, useScroll, useSpring, useTransform } from "framer-motion";
 
-
-
-//style={{ scaleX }} 
+//style={{ scaleX }}
 const BannerImages = [
-  [{url: banner_first,},{url: banner_second,},{url: banner_third,},],
-  [{url: banner_second,},{url: banner_third,},{url: banner_first,},],
-  [{url: banner_third,},{url: banner_first,},{url: banner_second,},],
+  [{ url: banner_first }, { url: banner_second }, { url: banner_third }],
+  [{ url: banner_second }, { url: banner_third }, { url: banner_first }],
+  [{ url: banner_third }, { url: banner_first }, { url: banner_second }],
 ];
 
-export default function Banner(){
-
+export default function Banner() {
   const { scrollYProgress } = useScroll();
-  
 
-  const scrollLeft = useSpring(useTransform(scrollYProgress, [0, 0.2], [100, -200]), {
-    stiffness: 100,
-    damping: 30,
-    restDelta: 0.001
-  });
-  const scrollCenter = useSpring(useTransform(scrollYProgress, [0, 0.5], [-400, 200]), {
-    stiffness: 100,
-    damping: 30,
-    restDelta: 0.001
-  });
-  const scrollRight = useSpring(useTransform(scrollYProgress, [0, 0.2], [-50, -300]), {
-    stiffness: 100,
-    damping: 30,
-    restDelta: 0.001
-  });
+  const scrollLeft = useSpring(
+    useTransform(scrollYProgress, [0, 0.2], [100, -200]),
+    {
+      stiffness: 100,
+      damping: 30,
+      restDelta: 0.001,
+    }
+  );
+  const scrollCenter = useSpring(
+    useTransform(scrollYProgress, [0, 0.5], [-400, 200]),
+    {
+      stiffness: 100,
+      damping: 30,
+      restDelta: 0.001,
+    }
+  );
+  const scrollRight = useSpring(
+    useTransform(scrollYProgress, [0, 0.2], [-50, -300]),
+    {
+      stiffness: 100,
+      damping: 30,
+      restDelta: 0.001,
+    }
+  );
+
   return (
     <section className="w-full h-[300px] bg-[#F2F2F5] overflow-hidden relative">
       <motion.div className="flex gap-6 justify-center items-center rotate-12">
-        <motion.div className="flex flex-col justify-center items-center gap-6 grow" style={{ y: scrollLeft }}>
+        <motion.div
+          className="flex flex-col justify-center items-center gap-6 grow"
+          style={{ y: scrollLeft }}
+        >
           {BannerImages[1].map((BannerImage, i) => {
             return (
-            <motion.div key={i} className="w-full h-[200px]"  
-              style={{ backgroundImage: `url(${BannerImage.url})`, 
-                      backgroundSize: "cover",
-                      backgroundPosition: "top center",
-                    }} />
-            )
+              <motion.div
+                key={i}
+                className="w-full h-[200px]"
+                style={{
+                  backgroundImage: `url(${BannerImage.url})`,
+                  backgroundSize: "cover",
+                  backgroundPosition: "top center",
+                }}
+              />
+            );
           })}
         </motion.div>
-        <motion.div className="flex flex-col justify-center items-center gap-6 grow" style={{ y: scrollCenter}}>
+        <motion.div
+          className="flex flex-col justify-center items-center gap-6 grow"
+          style={{ y: scrollCenter }}
+        >
           {BannerImages[1].map((BannerImage, i) => {
             return (
-            <motion.div key={i} className="w-full h-[200px]"  
-              style={{ backgroundImage: `url(${BannerImage.url})`, 
-                      backgroundSize: "cover",
-                      backgroundPosition: "top center",
-                    }} />
-            )
+              <motion.div
+                key={i}
+                className="w-full h-[200px]"
+                style={{
+                  backgroundImage: `url(${BannerImage.url})`,
+                  backgroundSize: "cover",
+                  backgroundPosition: "top center",
+                }}
+              />
+            );
           })}
         </motion.div>
-        <motion.div className="flex flex-col justify-center items-center gap-6 grow" style={{ y: scrollRight}}>
+        <motion.div
+          className="flex flex-col justify-center items-center gap-6 grow"
+          style={{ y: scrollRight }}
+        >
           {BannerImages[2].map((BannerImage, i) => {
             return (
-            <motion.div key={i} className="w-full h-[200px]"  
-              style={{ backgroundImage: `url(${BannerImage.url})`, 
-                      backgroundSize: "cover",
-                      backgroundPosition: "top center",
-                    }} />
-            )
+              <motion.div
+                key={i}
+                className="w-full h-[200px]"
+                style={{
+                  backgroundImage: `url(${BannerImage.url})`,
+                  backgroundSize: "cover",
+                  backgroundPosition: "top center",
+                }}
+              />
+            );
           })}
         </motion.div>
       </motion.div>
@@ -74,4 +101,3 @@ export default function Banner(){
     </section>
   );
 }
-
